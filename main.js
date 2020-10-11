7d123c0bd25aa7ae6b4f5fbb5c7b6f7d2bdfaf02
Damien <damienbanville@hotmail.fr>
2020-10-11 19:27:34 +0200

    modal done and front changed

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const api_key = "&apikey=9d0b481b";
 const selector = document.getElementsByClassName("row movies")[0];
 const selector_movie = document.getElementsByClassName("ok")[0];
 const showMovie = (selector, title, released, poster, numb) => {
-    var detail_url =`https://www.omdbapi.com/?t=${title}&apikey=9d0b481b`;
+    const detail_url =`https://www.omdbapi.com/?t=${title}&apikey=9d0b481b`;
+    console.log("lalalal");
     selector.innerHTML += `
+          <div class="col-2"></div>
 
-          <div class="row border pt-2 pb-2 rounded mb-3 h-10 w-75">
+          <div class="row border pt-2 pb-2 bg-light rounded mb-3 h-10 w-75">
             <div class="col-2">
               <img src="${poster}" height="150px" alt="Poster">
             </div>
@@ -14,32 +16,38 @@ const showMovie = (selector, title, released, poster, numb) => {
             <div class="col-9">
               <h2>${title}</h2>
               <p>${released}</p>
-              <p>${numb} </p>
-              <a href="#" onclick="readMore(\'${detail_url}\', ${numb})">En savoir plus</a>
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter${numb}">
+                En savoir plus
+              </button>
+
 
             </div>
           </div>
+
+          <div class="modal fade-lg" id="exampleModalCenter${numb}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered-lg" role="document">
+              <div class="modal-content">
+
+                <div class="modal-body${numb}">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                  ${readMore(detail_url, numb)}
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                </div>
+              </div>
+            </div>
+          </div>
     `
+    readMore(detail_url, numb);
+
 }
 
 const showMovieDetails = (selector_movie, title, released, plot,poster) => {
-    selector_movie.innerHTML += `
-
-          <div class="row border pt-2 pb-2 rounded mb-3 h-10 w-75">
-            <div class="col-4">
-              <img src="${poster}" height="300px" alt="Poster">
-            </div>
-
-            <div class="col-8">
-              <h2>${title}</h2>
-              <p>${released}</p>
-              <p>${plot}</p>
-              <a href="javascript:back()">Retour</a>
 
 
-            </div>
-          </div>
-    `
 }
 
 const getMovies = async (request_URL_final) => {
@@ -96,7 +104,7 @@ function back() {
   x.removeChild(x.lastChild);
 }
 
-function readMore(detail_url, numb, e){
+function readMore(detail_url, numb){
   console.log('ici');
   console.log(numb);
   const selector_movie = document.getElementsByClassName("row movies")[0];
@@ -110,6 +118,24 @@ function readMore(detail_url, numb, e){
           console.log(released);
           const poster = element.Poster;
           const plot = element.Plot;
-          showMovieDetails(selector_movie,title, released, plot, poster);
-      });
+          const movie_information = [];
+          movie_information.push(title, released, plot, poster);
+          console.log("movie info");
+          console.log(movie_information);
+          document.getElementsByClassName(`modal-body${numb}`)[0].innerHTML = `
+          <div class="row mb-3 mt-3">
+            <div class="col-4 ml-3 ">
+              <img src="${poster}" height="300px" class="rounded" alt="Poster">
+            </div>
+            <div class="col-1"></div>
+            <div class="col-6">
+               <h3>${title}</h3> <br>
+               <h6><small class="text-muted"> ${released} </small></h6>
+                <br>  <h6>${plot}</h6>
+            </div>
+
+          </div>
+          `;
+          // showMovieDetails(selector_movie,title, released, plot, poster);
+      }).then();
 };
